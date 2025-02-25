c4ec415437f04575fec90c872c69e343fa783bb5
iko2000 <shengelia1800@gmail.com>
2025-02-25 20:25:14 +0100

    scrhool added

--- app/integration/page.tsx
+++ app/integration/page.tsx
@@ -1,26 +1,75 @@
-'use client'
+import InfiniteScroll from "@/components/reactbits/advanced-coll";
+import { FaLightbulb } from "react-icons/fa";
+
+const items:any = [
+  {content: (
+    <div className="w-full bg-blue-50 rounded-2xl p-5 flex items-center space-x-4">
+        <FaLightbulb className="text-blue-600 text-2xl" />
+        <p className="text-blue-700">Define your needs and goals for the integration.</p>
+    </div>
+)},
+{content: (
+  <div className="w-full h-full bg-blue-50 rounded-2xl p-5 flex items-center space-x-4">
+      <FaLightbulb className="text-blue-600 text-2xl" />
+      <p className="text-blue-700">Define your needs and goals for the integration.</p>
+  </div>
+)}
+,
+{content: (
+  <div className="w-full h-full bg-blue-50 rounded-2xl p-5 flex items-center space-x-4">
+      <FaLightbulb className="text-blue-600 text-2xl" />
+      <p className="text-blue-700">Define your needs and goals for the integration.</p>
+  </div>
+)}
+,
+{content: (
+  <div className="w-full h-full bg-blue-50 rounded-2xl p-5 flex items-center space-x-4">
+      <FaLightbulb className="text-blue-600 text-2xl" />
+      <p className="text-blue-700">Define your needs and goals for the integration.</p>
+  </div>
+)}
+,
+{content: (
+  <div className="w-full h-full bg-blue-50 rounded-2xl p-5 flex items-center space-x-4">
+      <FaLightbulb className="text-blue-600 text-2xl" />
+      <p className="text-blue-700">Define your needs and goals for the integration.</p>
+  </div>
+)}
+,
+{content: (
+  <div className="w-full h-full bg-blue-50 rounded-2xl p-5 flex items-center space-x-4">
+      <FaLightbulb className="text-blue-600 text-2xl" />
+      <p className="text-blue-700">Define your needs and goals for the integration.</p>
+  </div>
+)}
+,
+{content: (
+  <div className="w-full h-full bg-blue-50 rounded-2xl p-5 flex items-center space-x-4">
+      <FaLightbulb className="text-blue-600 text-2xl" />
+      <p className="text-blue-700">Define your needs and goals for the integration.</p>
+  </div>
+)}
+
+
+];
+  
+
 import React from 'react'
-import Image from 'next/image'
-const handleAnimationComplete = () => {
-  console.log('Animation completed!');
-};
 
-
-
-export default function InfrastructurePage() {
+export default function Integration() {
   return (
-    <>
-      <div className="w-screen h-screen flex justify-center items-center flex-col">
-        {/* BlurText Component */}
-       
-
-        {/* LetterGlitch Container */}
-        <div className="w-full max-w-4xl h-96 flex justify-center items-center"> 
-          {/* Adjusted container size */}
-         
-        </div>
-      </div>
-    </>
-  );
+    <section className="w-full h-full">
+      <div style={{height: '1000px', position: 'relative'}}>
+  <InfiniteScroll
+    items={items}
+    isTilted={true}
+    tiltDirection='left'
+    autoplay={true}
+    autoplaySpeed={0.1}
+    autoplayDirection="down"
+    pauseOnHover={true}
+  />
+</div>
+    </section>
+  )
 }
-
