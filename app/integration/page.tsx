991e7cacd6e392201853a080c5df61601009acfa
iko2000 <shengelia1800@gmail.com>
2025-02-25 13:13:26 +0100

    integration

--- app/integration/page.tsx
+++ app/integration/page.tsx
@@ -12,14 +12,7 @@ export default function InfrastructurePage() {
     <>
       <div className="w-screen h-screen flex justify-center items-center flex-col">
         {/* BlurText Component */}
-        <BlurText
-          text="Isn't this so cool?!"
-          delay={150}
-          animateBy="words"
-          direction="top"
-          onAnimationComplete={handleAnimationComplete}
-          className="text-4xl mb-12" // Increased text size and margin
-        />
+       
 
         {/* LetterGlitch Container */}
         <div className="w-full max-w-4xl h-96 flex justify-center items-center"> 
