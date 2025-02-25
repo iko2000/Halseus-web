9ab9cbd8e9bf44a134a88dd997db31ea8eb69d51
iko2000 <shengelia1800@gmail.com>
2025-02-25 19:38:24 +0100

    quality

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -8,7 +8,7 @@ const ProductsPage = () => {
 
     const demoItems = [
         { link: '/products/ai-gpt', text: 'Customized ChatGPT', image: "https://images.pexels.com/photos/16245252/pexels-photo-16245252/free-photo-of-a-laptop-computer-with-the-word-chatgpt-on-it.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
-        { link: '/products/ai-training', text: 'Train AI model on Specific Data', image: 'https://images.pexels.com/photos/965345/pexels-photo-965345.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
+        { link: '/products/ai-training', text: 'Train AI model', image: 'https://images.pexels.com/photos/965345/pexels-photo-965345.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
         { link: '/products/ai-local', text: 'Local AI', image: 'https://images.pexels.com/photos/60504/security-protection-anti-virus-software-60504.jpeg' },
     ];
 
