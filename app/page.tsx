beeb1511cd2a8e4c3ef04a817e16a9647451eca1
iko2000 <shengelia1800@gmail.com>
2025-03-07 16:03:45 +0100

    seo optimization

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,45 @@ import WhyCustomAIModels from "@/components/homepage/explanation";
 import AILocalBenefits from "@/components/homepage/Ai-benefits";
 import ourai from '../public/assets/halseus.png';
 
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "http://localhost:3000";
+
+  export const metadata = {
+    metadataBase: new URL(defaultUrl),
+    title: "Halseus | Local AI Solutions",
+    description: "Halseus offers cutting-edge local AI solutions for businesses and individuals.",
+    icons: {
+      icon: '/favicon.ico',
+    },
+    keywords: ["AI improve", "AI custom", "Local AI", "AI Solutions", "Halseus"],
+    openGraph: {
+      title: "Halseus | Local AI Solutions",
+      description: "Halseus offers cutting-edge local AI solutions for businesses and individuals.",
+      url: defaultUrl,
+      siteName: "Halseus",
+      images: [
+        {
+          url: `${defaultUrl}/assets/open-graph.png`, 
+          width: 1200,
+          height: 630,
+          alt: "Halseus - AI Solutions",
+        },
+      ],
+      locale: "en_US",
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: "Halseus | Local AI Solutions",
+      description: "Halseus offers cutting-edge local AI solutions for businesses and individuals.",
+      images: [`${defaultUrl}/assets/open-graph.png`], // Replace with actual image
+    },
+    alternates: {
+      canonical: defaultUrl,
+    },
+  };
+
 export default async function Home() {
 
 
